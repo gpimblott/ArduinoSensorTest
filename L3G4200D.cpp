4accde388d52a207f8a88ecc5e7c9722dc27c20c
gpimblott <gordon@pimblott.com>
2013-11-01 20:28:10 +0000

    Update licenses

--- L3G4200D.cpp
+++ L3G4200D.cpp
@@ -1,3 +1,20 @@
+/*
+L3G4200D.cpp - L3G4200D Gyroscope Arduino Library.
+Copyright (C) 2013 G.Pimblott
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include "L3G4200D.h"
 #include <Wire.h>
 #include <math.h>
--- L3G4200D.h
+++ L3G4200D.h
@@ -1,3 +1,20 @@
+/*
+L3G4200D.h - Header file for the L3G4200D Gyroscope Arduino Library.
+Copyright (C) 2013 G.Pimblott
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #ifndef L3G4200D_h
 #define L3G4200D_h
 
